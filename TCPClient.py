ab9cd9305def6ce7b91cad3ec519dcc5f4c62193
Henri Thomas <hmgdthomas@gmail.com>
2018-10-31 19:28:59 -0800

    10/31/18

--- TCPClient.py
+++ TCPClient.py
@@ -4,7 +4,7 @@ Henri Thomas
 Computer Networks
 Assignment 2
 """
-from socket import *
+#from socket import *
 import socket as s
 from TCPPacket import TCPPacket
 from TCPWindow import TCPWindow
@@ -15,10 +15,10 @@ from pathlib import Path
 from random import randint
 #STATES: CLOSED, SYN_SENT, ESTABLISHED, FIN_WAIT_1,
 #        FIN_WAIT_2, TIME_WAIT
-
+#192.168.1.109
 parser = argparse.ArgumentParser(description = "TCP-FTP Client - Henri Thomas")
 parser.add_argument('-p',"--port",action="store",type=int,help="Server's port number")
-parser.add_argument('-f',"--file",action="store",help="Client's requested file to read/write to/from the server")
+parser.add_argument('-f',"--file",action="store",help="Client's requested file to read/write from/to the server")
 parser.add_argument('-m',"--mode",choices={'r','w'},help="Client's mode to either read or write to/from the server")
 parser.add_argument('-i',"--ip",action="store",help="Client's IP address")
 args = parser.parse_args()
@@ -26,6 +26,7 @@ args = parser.parse_args()
 #CLOSED
 print('client - closed')
 server_port = args.port
+client_port = 0
 file_name = args.file
 if args.mode == 'r':
     writing = False
@@ -36,43 +37,55 @@ established = False
 error = False
 block_size = 1024
 timeout = 0.5
-window_size = randint(4,9)
-client_isn = 200
+window_size = 0
+client_seq = 200
+server_seq = 0
+ack = 0
+urg = False
 received = bytes()
 sending = bytes()
 client_packet_manager = TCPPacket()
 
-client_socket = socket(AF_INET,SOCK_DGRAM)
+client_socket = s.socket(s.AF_INET,s.SOCK_DGRAM)
 client_socket.settimeout(timeout)
 server_address = (host,server_port)
 #client_socket.connect((host,server_port))
 
 if writing:
+    urg = True
     f = open(file_name, 'rb')
-    sending = client_packet_manager.create_syn_wrq_packet(file_name,0,server_port,client_isn,window_size)
+    sending = client_packet_manager.create_syn_wrq_packet(file_name,0,server_port,client_seq,window_size)
     client_socket.sendto(sending,server_address)
-    print('client - write syn sent')
-    client_packet_manager.print_self()
+    window_size = randint(4,9)
+    print('client - write syn sent - seq:',client_seq)
+    #client_packet_manager.print_self()
 else:
     file_test = Path(file_name)
     if file_test.is_file():
-        f = open(file_name + 'CLIENT', 'wb')
-    sending = client_packet_manager.create_syn_rrq_packet(file_name,0,server_port,client_isn)
+        f = open(file_name + 'CLIENT', 'wb')        
+    sending = client_packet_manager.create_syn_rrq_packet(file_name,0,server_port,client_seq)
     client_socket.sendto(sending,server_address)
-    print('client - read syn sent')
-    client_packet_manager.print_self()
-
+    print('client - read syn sent - seq:',client_seq)
+    
 #SYN_SENT
-print('client - syn sent')
 try:
     received, server = client_socket.recvfrom(block_size)
-    client_packet_manager.deconstruct_packet(received)
-    established = True
-    print('client - server address check: ip',server[0],' port',server[1])
-    print('client - synack recevied:')
-    client_packet_manager.print_self()
-except timeout:
+except s.timeout:
     print('client - synack timeout, closing connection')
+    f.close()
+    client_socket.close()
+client_packet_manager.deconstruct_packet(received)
+established = True
+client_port = client_packet_manager.destination_port.uint
+server_seq = client_packet_manager.sequence_number.uint
+client_seq = client_packet_manager.ack_number.uint
+print('client - server address: ip:',server[0],' port:',server[1])
+print('client - synack recevied')
+#SEND ACK
+ack = server_seq + 1
+sending = client_packet_manager.create_ack_packet(client_port,server_port,client_seq,ack,urg,window_size)
+client_socket.sendto(sending,server_address)
+print('client - synack ack sent - seq:',client_seq,' ack:',ack)
 
 #ESTABLISHED
 if established:
--- TCPServer.py
+++ TCPServer.py
@@ -4,7 +4,7 @@ Henri Thomas
 Computer Networks
 Assignment 2
 """
-from socket import *
+#from socket import *
 import socket as s
 from TCPPacket import TCPPacket
 from TCPWindow import TCPWindow
@@ -22,45 +22,58 @@ args = parser.parse_args()
 
 #CLOSED
 server_port = args.port
+client_port = 0
 host = s.gethostname()
 established = False
 error = False
 block_size = 1024
 timeout = 0.5
 window_size = 0
-server_isn = 300
+server_seq = 300
+client_seq = 0
+ack = 0
 received = bytes()
 sending = bytes()
 server_packet_manager = TCPPacket()
 
 #LISTEN
-server_socket = socket(AF_INET,SOCK_DGRAM)
+server_socket = s.socket(s.AF_INET,s.SOCK_DGRAM)
+print('server - ready to receive')
 server_socket.bind((host,server_port))
 #server_socket.settimeout(timeout)
 
 #SYN_RCVD
 received, client = server_socket.recvfrom(block_size)
 server_packet_manager.deconstruct_packet(received)
-established = True
-print('server - client address check: ip',client[0],' port',client[1])
-    
-client_seq = server_packet_manager.sequence_number
+print('server - client address: ip:',client[0],' port:',client[1])
+client_port = client[1]
+client_seq = server_packet_manager.sequence_number.uint
 client_control_bits = server_packet_manager.control
 if client_control_bits[0] == True:
     urg = True
 else:
     urg = False
     window_size = randint(4,9)
-client_window_size = server_packet_manager.window.int
+client_window_size = server_packet_manager.window.uint
 
-sending = server_packet_manager.create_synack_packet(server_port,0,server_isn,(client_seq + block_size),urg,window_size)
+ack = client_seq + 1
+sending = server_packet_manager.create_synack_packet(server_port,client_port,server_seq,ack,urg,client_window_size)
 server_socket.sendto(sending,client)
+print('server - synack sent - seq:',server_seq,'ack:',ack)
 
+received, client = server_socket.recvfrom(block_size)
+server_packet_manager.deconstruct_packet(received)
+established = True
+print('server - synack ack received')
 #ESTABLISHED
-
+if established:
+    print('server - established')
+    
 #CLOSE_WAIT
+print('server - close wait')
 
 #lAST_ACK
+print('server - last ack')
 
 #CLOSED
 server_socket.close()
